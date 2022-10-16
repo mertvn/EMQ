e5841d1c35d700621bec9ab50ff16cfe8c9d63ec
mertvn <mertvn@protonmail.com>
2022-10-16 12:46:29 +0300

    Initial commit

---
+++ BlazorApp1/Shared/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Shared;
+
+public static class Utils
+{
+    public static string Base64Encode(this string plainText)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static string Base64Decode(this string base64EncodedData)
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+}
