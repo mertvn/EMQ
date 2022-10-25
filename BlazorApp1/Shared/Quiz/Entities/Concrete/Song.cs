00dd0bc7f7cb4150061802761030dc7a098a82c8
mertvn <mertvn@protonmail.com>
2022-10-25 22:21:37 +0300

    Minor

--- BlazorApp1/Shared/Quiz/Entities/Concrete/Song.cs
+++ BlazorApp1/Shared/Quiz/Entities/Concrete/Song.cs
@@ -6,15 +6,13 @@ namespace BlazorApp1.Shared.Quiz.Entities.Concrete;
 
 public class Song
 {
-    public string Name { get; set; }
+    public string Title { get; set; }
 
     public string Artist { get; set; }
 
     public string[] Links { get; set; } // todo multiple links
 
     [JsonIgnore] public string Data { get; set; }
-
-    // public DateTime Date { get; set; } // todo should this be here, or SongSource, or both
 
     // todo type
 
