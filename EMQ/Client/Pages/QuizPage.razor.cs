mertvn/EMQ

commit e6ff2f4f4cbfdb08cfc323d37c8fc3d7ca458dd1

Get rid of ClientState.Session.RoomId

EMQ/Client/Pages/QuizPage.razor.cs
@@ -166,7 +166,7 @@ public partial class QuizPage
     public async Task<Song?> NextSong(int index)
     {
         HttpResponseMessage res = await _client.PostAsJsonAsync("Quiz/NextSong",
-            new ReqNextSong(ClientState.Session!.RoomId!.Value, index));
+            new ReqNextSong(ClientState.Session!.Token,index));
         if (res.IsSuccessStatusCode)
         {
             ResNextSong? nextSong = await res.Content.ReadFromJsonAsync<ResNextSong>().ConfigureAwait(false);

EMQ/Client/ClientUtils.cs
@@ -36,7 +36,7 @@ public class ClientUtils
 
     public async Task<Room?> SyncRoom()
     {
-        var room = await Client.GetFromJsonAsync<Room>($"Quiz/SyncRoom?roomId={ClientState.Session?.RoomId}");
+        var room = await Client.GetFromJsonAsync<Room>($"Quiz/SyncRoom?token={ClientState.Session?.Token}");
         // Console.WriteLine(JsonSerializer.Serialize(room));
 
         if (room is not null)

EMQ/Client/Components/ChatComponent.razor.cs
@@ -34,11 +34,11 @@ public partial class ChatComponent
             _preventDefault = true;
 
             var session = ClientState.Session;
-            if (session is { RoomId: { } } && !string.IsNullOrWhiteSpace(ChatInputText))
+            if (session != null && !string.IsNullOrWhiteSpace(ChatInputText))
             {
-                if (ChatInputText.Length < Constants.MaxChatMessageLength)
+                if (ChatInputText.Length <= Constants.MaxChatMessageLength)
                 {
-                    var req = new ReqSendChatMessage(session.Token, session.RoomId.Value, ChatInputText);
+                    var req = new ReqSendChatMessage(session.Token, ChatInputText);
                     ChatInputText = "";
                     var res = await _client.PostAsJsonAsync("Quiz/SendChatMessage", req);
                     if (res.IsSuccessStatusCode)

EMQ/Client/Pages/HotelPage.razor.cs
@@ -75,7 +75,6 @@ public partial class HotelPage
                 await Task.Delay(waitTime);
             }
 
-            ClientState.Session.RoomId = roomId;
             await _clientUtils.SaveSessionToLocalStorage();
 
             Navigation.NavigateTo("/RoomPage");

EMQ/Server/Controllers/QuizController.cs
@@ -30,12 +30,19 @@ public class QuizController : ControllerBase
 
     [HttpGet]
     [Route("SyncRoom")]
-    public Room? SyncRoom([FromQuery] int roomId)
+    public Room? SyncRoom([FromQuery] string token)
     {
-        var room = ServerState.Rooms.SingleOrDefault(x => x.Id == roomId);
+        var session = ServerState.Sessions.Single(x => x.Token == token);
+        if (session is null)
+        {
+            // todo
+            throw new Exception();
+        }
+
+        var room = ServerState.Rooms.SingleOrDefault(x => x.Players.Any(y => y.Id == session.Player.Id));
         if (room is null)
         {
-            _logger.LogError("Room not found: " + roomId);
+            _logger.LogError("Room not found: " + token);
             return null;
         }
 
@@ -47,9 +54,14 @@ public class QuizController : ControllerBase
     [Route("NextSong")]
     public ActionResult<ResNextSong> NextSong([FromBody] ReqNextSong req)
     {
-        // todo? verify user belongs in room
-        var room = ServerState.Rooms.SingleOrDefault(x => x.Id == req.RoomId);
+        var session = ServerState.Sessions.Single(x => x.Token == req.PlayerToken);
+        if (session is null)
+        {
+            // todo
+            throw new Exception();
+        }
 
+        var room = ServerState.Rooms.SingleOrDefault(x => x.Players.Any(y => y.Id == session.Player.Id));
         if (room is not null)
         {
             if (room.Quiz != null)
@@ -76,14 +88,14 @@ public class QuizController : ControllerBase
             }
             else
             {
-                _logger.LogError("Room does not have a quiz initialized: " + req.RoomId);
-                return BadRequest("Room does not have a quiz initialized: " + req.RoomId);
+                _logger.LogError("Room does not have a quiz initialized: " + room.Id);
+                return BadRequest("Room does not have a quiz initialized: " + room.Id);
             }
         }
         else
         {
-            _logger.LogError("Room not found: " + req.RoomId);
-            return BadRequest("Room not found: " + req.RoomId);
+            _logger.LogError("Room not found with playerToken: " + req.PlayerToken);
+            return BadRequest("Room not found with playerToken: " + req.PlayerToken);
         }
     }
 
@@ -303,13 +315,13 @@ public class QuizController : ControllerBase
         }
 
         var player = session.Player;
-        var room = ServerState.Rooms.Find(x => x.Id == req.RoomId);
+        var room = ServerState.Rooms.Find(x => x.Players.Any(y => y.Id == player.Id));
 
         if (room is not null)
         {
             if (room.Players.Any(x => x.Id == player.Id))
             {
-                if (req.Contents.Length < Constants.MaxChatMessageLength)
+                if (req.Contents.Length <= Constants.MaxChatMessageLength)
                 {
                     var chatMessage = new ChatMessage(req.Contents, player);
                     room.Chat.Enqueue(chatMessage);
@@ -321,12 +333,12 @@ public class QuizController : ControllerBase
             {
                 _logger.LogWarning(
                     "Attempt to send chat message to r{req.RoomId} which p{player.Id} does not belong to",
-                    req.RoomId, player.Id);
+                    room.Id, player.Id);
             }
         }
         else
         {
-            _logger.LogWarning("Attempt to send chat message to r{req.RoomId} which is null", req.RoomId);
+            _logger.LogWarning("Attempt to send chat message to r{req.RoomId} which is null", room.Id);
         }
     }
 }

EMQ/Shared/Auth/Entities/Concrete/Session.cs
@@ -24,10 +24,7 @@ public class Session
     [JsonIgnore]
     public HubConnection? hubConnection { get; set; }
 
-    public int? RoomId { get; set; }
-
     public PlayerVndbInfo VndbInfo { get; set; } = new();
 
     public DateTime CreatedAt { get; }
-
 }

EMQ/Shared/Quiz/Entities/Concrete/Dto/Request/ReqNextSong.cs
@@ -2,13 +2,13 @@
 
 public class ReqNextSong
 {
-    public ReqNextSong(int roomId, int songIndex)
+    public ReqNextSong(string playerToken, int songIndex)
     {
-        RoomId = roomId;
+        PlayerToken = playerToken;
         SongIndex = songIndex;
     }
 
-    public int RoomId { get; }
+    public string PlayerToken { get; }
 
     public int SongIndex { get; }
 }

EMQ/Shared/Quiz/Entities/Concrete/Dto/Request/ReqSendChatMessage.cs
@@ -2,16 +2,13 @@
 
 public class ReqSendChatMessage
 {
-    public ReqSendChatMessage(string playerToken, int roomId, string contents)
+    public ReqSendChatMessage(string playerToken, string contents)
     {
         PlayerToken = playerToken;
-        RoomId = roomId;
         Contents = contents;
     }
 
     public string PlayerToken { get; set; }
-
-    public int RoomId { get; set; }
 
     public string Contents { get; set; }
 }

commit a5c362ec8a4bdaaf9bcb37631aa69feef01d69ad

Fix leaving quiz/room broken in e6ff2f4

EMQ/Client/Pages/QuizPage.razor.cs
@@ -313,14 +313,14 @@ public partial class QuizPage
 
     private async Task Onclick_Leave()
     {
-        await SyncWithServer();
+        // await SyncWithServer();
 
         bool confirmed = await _jsRuntime.InvokeAsync<bool>("confirm",
             "Really leave? If you return your score will not be restored.");
         if (confirmed)
         {
             await ClientState.Session!.hubConnection!.SendAsync("SendPlayerLeaving");
-            await SyncWithServer();
+            // await SyncWithServer();
 
             // i have no idea why, but if we don't visit RoomPage first, the next room a player enters will have double timer tick etc.
             _navigation.NavigateTo("/RoomPage");

EMQ/Client/Pages/RoomPage.razor.cs
@@ -146,13 +146,13 @@ public partial class RoomPage
 
     private async Task Onclick_Leave()
     {
-        Room = await _clientUtils.SyncRoom();
+        // Room = await _clientUtils.SyncRoom();
 
         bool confirmed = await _jsRuntime.InvokeAsync<bool>("confirm", "Really leave?");
         if (confirmed)
         {
             await ClientState.Session!.hubConnection!.SendAsync("SendPlayerLeaving");
-            Room = await _clientUtils.SyncRoom();
+            // Room = await _clientUtils.SyncRoom();
             Navigation.NavigateTo("/HotelPage");
         }
     }