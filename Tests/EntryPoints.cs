07109e89c8eda97e45a51ca5bf434c8f12971999
mertvn <mertvn@protonmail.com>
2023-03-01 18:51:26 +0300

    minor

--- Tests/EntryPoints.cs
+++ Tests/EntryPoints.cs
@@ -87,7 +87,7 @@ public class EntryPoints
     [Test, Explicit]
     public async Task ApproveReviewQueueItem()
     {
-        var rqIds = Enumerable.Range(62, 60).ToArray();
+        var rqIds = Enumerable.Range(79, 60).ToArray();
 
         foreach (int rqId in rqIds)
         {
