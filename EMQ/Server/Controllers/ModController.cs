702af6c8a1adc7d864cd9ba690b485364e18033f
mertvn <mertvn@protonmail.com>
2023-03-04 14:53:36 +0300

    testing something

---
+++ EMQ/Server/Controllers/ModController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using EMQ.Server.Db;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EMQ.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ModController : ControllerBase
+{
+    public ModController(ILogger<ModController> logger)
+    {
+        _logger = logger;
+    }
+
+    private readonly ILogger<ModController> _logger;
+
+    [HttpGet]
+    [Route("ExportSongLite")]
+    public async Task<string> ExportSongLite([FromQuery] string adminPassword)
+    {
+        string? envVar = Environment.GetEnvironmentVariable("EMQ_ADMIN_PASSWORD");
+        if (string.IsNullOrWhiteSpace(envVar) || envVar != adminPassword)
+        {
+            Unauthorized();
+        }
+
+        return await DbManager.ExportSongLite();
+    }
+}
