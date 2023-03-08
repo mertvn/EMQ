17a5fb2dba86edf88289b646c617e2c4435a8812
mertvn <mertvn@protonmail.com>
2023-03-08 20:14:06 +0300

    Juliet: simplify ToJsonNormalized (maybe)

--- Juliet/Model/Filters/Query.cs
+++ Juliet/Model/Filters/Query.cs
@@ -3,57 +3,52 @@
 // https://code.blicky.net/yorhel/vndb/src/commit/29bccb3a5f661dc60eee1a8b3c24c776aa8c30c6/lib/VNWeb/AdvSearch.pm#L36
 public abstract class Query
 {
-    // todo maybe this should accept a list of queries?
     public static string ToJsonNormalized(Query query, ref string ret, bool isRoot)
     {
-        if (query is Combinator cc)
+        var list = new List<Query>();
+        switch (query)
         {
-            if (isRoot)
-            {
-                ret += "[";
-                ret += "\"" + cc.Kind.GetDescription() + "\"";
-                ret += ",";
-            }
-
-            foreach (Query cQuery in cc.Query)
-            {
-                if (ret.Any() && ret.Last() != ',')
+            case Combinator cc:
+                if (isRoot)
                 {
+                    ret += "[";
+                    ret += "\"" + cc.Kind.GetDescription() + "\"";
                     ret += ",";
                 }
 
-                switch (cQuery)
-                {
-                    case Combinator c:
-                        ret += "[" + "\"" + c.Kind.GetDescription() + "\"" + ",";
-                        ToJsonNormalized(c, ref ret, false);
-                        ret += "]";
-                        break;
-                    case Predicate p:
-                        ret +=
-                            "[" + "\"" + p.Name.GetDescription() + "\"" + "," +
-                            "\"" + p.Operator.GetDescription() + "\"" + "," +
-                            "\"" + p.Value + "\"" + "]";
-                        break;
-                }
-            }
-
-            if (isRoot)
-            {
-                ret += "]";
-            }
+                list.AddRange(cc.Query);
+                break;
+            case Predicate pp:
+                list.Add(pp);
+                break;
         }
-        else if (query is Predicate pp)
+
+        foreach (Query cQuery in list)
         {
             if (ret.Any() && ret.Last() != ',')
             {
                 ret += ",";
             }
 
-            ret +=
-                "[" + "\"" + pp.Name.GetDescription() + "\"" + "," +
-                "\"" + pp.Operator.GetDescription() + "\"" + "," +
-                "\"" + pp.Value + "\"" + "]";
+            switch (cQuery)
+            {
+                case Combinator c:
+                    ret += "[" + "\"" + c.Kind.GetDescription() + "\"" + ",";
+                    ToJsonNormalized(c, ref ret, false);
+                    ret += "]";
+                    break;
+                case Predicate p:
+                    ret +=
+                        "[" + "\"" + p.Name.GetDescription() + "\"" + "," +
+                        "\"" + p.Operator.GetDescription() + "\"" + "," +
+                        "\"" + p.Value + "\"" + "]";
+                    break;
+            }
+        }
+
+        if (isRoot && query is Combinator)
+        {
+            ret += "]";
         }
 
         return ret;
