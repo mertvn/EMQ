fb57e70eaf07bfa2214eb19cd7ba5eb9bdc227dd
mertvn <mertvn@protonmail.com>
2023-03-13 16:23:02 +0300

    meme commit please ignore

---
+++ EMQ/Client/Pages/Hasucana.razor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMQ.Client.Pages;
+
+// inside joke please ignore
+public partial class Hasucana
+{
+    private static Dictionary<string, string> Dict { get; set; } = new()
+    {
+        { "a", "の" },
+        { "e", "ま" },
+        { "g", "ム" },
+        { "h", "ん" },
+        { "l", "し" },
+        { "m", "ホ" },
+        { "o", "ロ" },
+        { "p", "ヤ" },
+        { "r", "イ" },
+        { "s", "ら" },
+        { "u", "ひ" },
+        { "v", "レ" },
+        { "y", "と" },
+    };
+
+    private string InputForEncode { get; set; } = "sa";
+
+    private string InputForDecode { get; set; } = "のら";
+
+    public static string Encode(string s)
+    {
+        var sb = new StringBuilder(s);
+        for (int index = 0; index < sb.Length; index++)
+        {
+            char c = sb[index];
+            if (Dict.ContainsKey(c.ToString()))
+            {
+                sb[index] = Dict[c.ToString()][0];
+            }
+            else
+            {
+                sb[index] = "?"[0];
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static string Decode(string s)
+    {
+        var sb = new StringBuilder(s);
+        for (int index = 0; index < sb.Length; index++)
+        {
+            char c = sb[index];
+            if (Dict.ContainsValue(c.ToString()))
+            {
+                sb[index] = Dict.Single(x => x.Value == c.ToString()).Key[0];
+            }
+            else
+            {
+                sb[index] = "?"[0];
+            }
+        }
+
+        return sb.ToString();
+    }
+}
