b9f603a784b45f58f82929c3dc3d657e62e6ac10
mertvn <mertvn@protonmail.com>
2023-03-19 12:04:13 +0300

    Add range validation to quiz filters

--- EMQ/Shared/Quiz/Entities/Concrete/QuizFilters.cs
+++ EMQ/Shared/Quiz/Entities/Concrete/QuizFilters.cs
@@ -24,20 +24,28 @@ public class QuizFilters
             $"End date must be in range of {Constants.QFDateMin} to {Constants.QFDateMax}")]
     public DateTime EndDateFilter { get; set; } = DateTime.Parse(Constants.QFDateMax);
 
+    [Range(Constants.QFRatingAverageMin, Constants.QFRatingAverageMax)]
     public int RatingAverageStart { get; set; } = Constants.QFRatingAverageMin;
 
+    [Range(Constants.QFRatingAverageMin, Constants.QFRatingAverageMax)]
     public int RatingAverageEnd { get; set; } = Constants.QFRatingAverageMax;
 
+    [Range(Constants.QFRatingBayesianMin, Constants.QFRatingBayesianMax)]
     public int RatingBayesianStart { get; set; } = Constants.QFRatingBayesianMin;
 
+    [Range(Constants.QFRatingBayesianMin, Constants.QFRatingBayesianMax)]
     public int RatingBayesianEnd { get; set; } = Constants.QFRatingBayesianMax;
 
+    [Range(Constants.QFPopularityMin, Constants.QFPopularityMax)]
     public int PopularityStart { get; set; } = Constants.QFPopularityMin;
 
+    [Range(Constants.QFPopularityMin, Constants.QFPopularityMax)]
     public int PopularityEnd { get; set; } = Constants.QFPopularityMax;
 
+    [Range(Constants.QFVoteCountMin, Constants.QFVoteCountMax)]
     public int VoteCountStart { get; set; } = Constants.QFVoteCountMin;
 
+    [Range(Constants.QFVoteCountMin, Constants.QFVoteCountMax)]
     public int VoteCountEnd { get; set; } = Constants.QFVoteCountMax;
 
     // todo move all applicable filters here
