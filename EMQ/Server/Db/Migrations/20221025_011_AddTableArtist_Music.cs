23c760993303148e6d791517d52c87453bface8a
mertvn <mertvn@protonmail.com>
2023-04-12 18:04:58 +0300

    Add artist_alias_id index on artist_music

--- EMQ/Server/Db/Migrations/20221025_011_AddTableArtist_Music.cs
+++ EMQ/Server/Db/Migrations/20221025_011_AddTableArtist_Music.cs
@@ -16,6 +16,7 @@ public class AddTableArtist_Music : Migration
             .WithColumn("artist_alias_id").AsInt32().NotNullable().ForeignKey("artist_alias", "id");
 
         Create.Index().OnTable(tableName).InSchema("public").OnColumn("music_id");
+        Create.Index().OnTable(tableName).InSchema("public").OnColumn("artist_alias_id");
     }
 
     public override void Down()
