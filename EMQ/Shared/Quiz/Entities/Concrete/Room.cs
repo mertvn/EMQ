aaa26630e55338c52b1c6bb908f674928f9826b0
mertvn <mertvn@protonmail.com>
2023-12-25 16:54:45 +0300

    experiment

--- EMQ/Shared/Quiz/Entities/Concrete/Room.cs
+++ EMQ/Shared/Quiz/Entities/Concrete/Room.cs
@@ -63,7 +63,7 @@ public sealed class Room : IDisposable
     public void Log(string message, int playerId = -1, bool writeToChat = false)
     {
         var roomLog = new RoomLog(Id, Quiz?.Id ?? Guid.Empty, QuizSettings, Quiz?.QuizState ?? null, playerId, message);
-        RoomLog.Enqueue(roomLog);
+        // RoomLog.Enqueue(roomLog); // todo
 
         Console.WriteLine(roomLog.ToString());
 
