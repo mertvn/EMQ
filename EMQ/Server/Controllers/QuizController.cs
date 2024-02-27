a77ac35306a7d4094c969825e76ce47196e41b51
mertvn <mertvn@protonmail.com>
2024-02-27 20:32:56 +0300

    allow room owners to go back in their rooms even without a password

--- EMQ/Server/Controllers/QuizController.cs
+++ EMQ/Server/Controllers/QuizController.cs
@@ -221,7 +221,7 @@ public class QuizController : ControllerBase
         }
 
         var player = session.Player;
-        if (string.IsNullOrWhiteSpace(room.Password) || room.Password == req.Password)
+        if (string.IsNullOrWhiteSpace(room.Password) || room.Password == req.Password || room.Owner == session.Player)
         {
             if (room.Players.Any(x => x.Id == player.Id) || room.Spectators.Any(x => x.Id == player.Id))
             {
