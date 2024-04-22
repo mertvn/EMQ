4783993f66ccd72de0622e924a6630576edcd9cf
mertvn <mertvn@protonmail.com>
2024-04-22 09:22:03 +0300

    backup botg database

--- EMQBackupScript/Program.cs
+++ EMQBackupScript/Program.cs
@@ -80,7 +80,7 @@ public static class Program
             File.Delete("output_pg_dump_song.txt");
             while (!proc.StandardOutput.EndOfStream)
             {
-                File.AppendAllText("output_pg_dump_auth.txt", proc.StandardOutput.ReadLine() + "\n");
+                File.AppendAllText("output_pg_dump_song.txt", proc.StandardOutput.ReadLine() + "\n");
             }
 
             if (!File.Exists(dumpFileName))
@@ -97,6 +97,53 @@ public static class Program
         catch (Exception e)
         {
             string message = $"EMQ SONG DATABASE BACKUP SCRIPT FAILED: {e}";
+            Console.WriteLine(message);
+            Process.Start("cmd.exe", $"/C msg %username% {message}");
+        }
+
+        try
+        {
+            Directory.SetCurrentDirectory(@"C:/emq/dbbackups/botg");
+            string envVar = "EMQ_REMOTE_BOTG_DATABASE_URL";
+
+            var builder = ConnectionHelper.GetConnectionStringBuilderWithEnvVar(envVar);
+            Environment.SetEnvironmentVariable("PGPASSWORD", builder.Password);
+
+            string dumpFileName = $"pgdump_{DateTime.UtcNow:yyyy-MM-dd}_{builder.Database}@{builder.Host}.tar";
+            var proc = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "pg_dump",
+                    Arguments =
+                        $"-U \"{builder.Username}\" -h \"{builder.Host}\" -p \"{builder.Port}\" -F \"t\" -f {dumpFileName} -d \"{builder.Database}\"",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                }
+            };
+            proc.Start();
+
+            File.Delete("output_pg_dump_botg.txt");
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                File.AppendAllText("output_pg_dump_botg.txt", proc.StandardOutput.ReadLine() + "\n");
+            }
+
+            if (!File.Exists(dumpFileName))
+            {
+                throw new Exception("pg_dump failed");
+            }
+
+            long filesizeBytes = new FileInfo(dumpFileName).Length;
+            if (filesizeBytes <= (7 * 1000)) // 7 KB
+            {
+                throw new Exception("filesizeBytes is too small");
+            }
+        }
+        catch (Exception e)
+        {
+            string message = $"EMQ BOTG DATABASE BACKUP SCRIPT FAILED: {e}";
             Console.WriteLine(message);
             Process.Start("cmd.exe", $"/C msg %username% {message}");
         }
