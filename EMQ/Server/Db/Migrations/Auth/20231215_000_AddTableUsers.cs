cc23c3c1eb82785ccfad2c61607e5c71ad17d12b
mertvn <mertvn@protonmail.com>
2024-05-08 09:35:50 +0300

    minor

--- EMQ/Server/Db/Migrations/Auth/20231215_000_AddTableUsers.cs
+++ EMQ/Server/Db/Migrations/Auth/20231215_000_AddTableUsers.cs
@@ -21,7 +21,7 @@ public class AddTableUsers : Migration
             .WithColumn("salt").AsString().NotNullable()
             .WithColumn("hash").AsString().NotNullable()
             .WithColumn("avatar").AsInt32().NotNullable().WithDefaultValue(Avatar.DefaultAvatar.Character)
-            .WithColumn("skin").AsString().NotNullable().WithDefaultValue(Avatar.DefaultAvatar.Skin);
+            .WithColumn("skin").AsString(32).NotNullable().WithDefaultValue(Avatar.DefaultAvatar.Skin);
 
         Execute.Sql(@"CREATE UNIQUE INDEX UC_users_username_lower ON users(lower(username));");
         Execute.Sql($@"ALTER TABLE users ADD CHECK (username ~* '{RegexPatterns.UsernameRegex}');");
