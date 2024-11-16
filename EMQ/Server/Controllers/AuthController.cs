67470695934ac1abffd03b8883928dd46334bebb
mertvn <mertvn@protonmail.com>
2024-11-16 07:12:00 +0300

    maybe this works?

--- EMQ/Server/Controllers/AuthController.cs
+++ EMQ/Server/Controllers/AuthController.cs
@@ -222,6 +222,7 @@ public class AuthController : ControllerBase
         {
             HttpContext.Response.Cookies.Append("user-id", session.Player.Id.ToString(), new CookieOptions
             {
+                Path = $".{Constants.WebsiteDomainNoProtocol}",
                 MaxAge = TimeSpan.FromDays(1),
                 Secure = true,
                 SameSite = SameSiteMode.Strict,
@@ -231,6 +232,7 @@ public class AuthController : ControllerBase
 
             HttpContext.Response.Cookies.Append("session-token", session.Token, new CookieOptions
             {
+                Path = $".{Constants.WebsiteDomainNoProtocol}",
                 MaxAge = TimeSpan.FromDays(1),
                 Secure = true,
                 SameSite = SameSiteMode.Strict,
