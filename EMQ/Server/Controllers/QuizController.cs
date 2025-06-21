87680e413eace7567ecdb6e9c04a93a43b0dfff1
mertvn <mertvn@protonmail.com>
2025-06-21 16:59:25 +0300

    minor

--- EMQ/Server/Controllers/QuizController.cs
+++ EMQ/Server/Controllers/QuizController.cs
@@ -233,7 +233,8 @@ public class QuizController : ControllerBase
         }
 
         var player = session.Player;
-        if (string.IsNullOrWhiteSpace(room.Password) || room.Password == req.Password || room.Owner == player)
+        if (string.IsNullOrWhiteSpace(room.Password) || room.Password == req.Password || room.Owner == player ||
+            AuthStuff.HasPermission(session, PermissionKind.Admin))
         {
             if (room.Players.Any(x => x.Id == player.Id) || room.Spectators.Any(x => x.Id == player.Id))
             {
