f705de6506c5736b9cdc570d68cef58abbdd2041
mertvn <mertvn@protonmail.com>
2025-10-08 23:38:38 +0300

    minor

--- EMQ/Shared/Core/RegexPatterns.cs
+++ EMQ/Shared/Core/RegexPatterns.cs
@@ -39,6 +39,7 @@ public static class RegexPatterns
         { "α", "a" },
         // { "β", "B" },
         { "βίος", "Bios" },
+        { "Rë∀˥", "Real" },
         { "Λ", "A" },
         { "∀", "A" },
         { "μ", "myu" },
